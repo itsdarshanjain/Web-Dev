214b2ace747333d9ebdaa22f0b0887b01a62cab8
Darshan Jain <darshanjaind14@gmail.com>
2026-07-15 19:28:18 +0530

    15 july

---
+++ javascript/function.js
@@ -0,0 +1,29 @@
+function heelo(){
+    //function declaration
+    console.log("Heelo world")
+}
+
+heelo();
+heelo();
+heelo()
+heelo()
+heelo()
+
+let hey=function(){
+    //function expression
+    console.log("Heeeeeeey");
+}
+hey();
+
+let fat= ()=>{
+    console.log("Fat arrow function")
+}
+fat();
+
+console.log("HelloWorld")
+console.log("Hello"+"World")
+console.log("Hello","World")
+console.log("Hello"-"World")
+console.log(`Hello World`)
+
+
