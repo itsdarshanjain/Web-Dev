7a7547473a9750e4579bb2d51aab42371feb1377
Darshan Jain <darshanjaind14@gmail.com>
2026-07-15 14:11:18 +0530

    15 JULY

--- javascript/divBy3and5..js
+++ javascript/divBy3and5..js
@@ -1,7 +1,38 @@
-let n=+window.prompt("Enter the no")
-let count=0;
-for(let i=1;i<=n;i++){
-    if(i%3===0&&i%5===0)    {console.log(i); count++
+// // let n=+window.prompt("Enter the no")
+// let count=0;
+// for(let i=1;i<=n;i++){
+//     if(i%3===0&&i%5===0)    {console.log(i); count++
+// }
+// // }
+// // console.log(count)
+
+// const a= document.getElementsByTagName("h1");
+// const b=document.getElementsByClassName("heading")
+// // a[0].style.color='blue'
+// b.style.color='blue';
+
+// ab();
+// function ab(){
+//     console.log("hello")
+// }
+
+// let age = 20;
+
+// if (age < 18) {
+//     throw new Error("You are not eligible.");
+// }
+
+// console.log("Welcome");
+
+try {
+    let age = 15;
+
+    if (age < 18) {
+        throw new Error("Age must be 18 or above.");
+    }
+
+    console.log("Welcome");
+
+} catch (err) {
+    console.log(err.message);
 }
-}
-console.log(count)
--- main.js
+++ main.js
@@ -1,1 +1,87 @@
 console.log("Darshan Jain");
+
+
+
+// async await 
+
+// async function add(a,b) {
+//     const result = await a+b
+//     console.log(result);
+    
+// }
+
+// add(10,20);
+
+// settime out 
+
+// console.log("Start");
+
+// setTimeout(()=>{
+//     console.log("Hiii");
+
+// },2000)
+
+// console.log("End");
+
+
+
+//  set interval
+
+// let count = 1;
+
+// const timer = setInterval(function () {
+
+//     console.log("Count:", count);
+
+//     count++;
+
+//     if (count > 5) {
+
+//         clearInterval(timer);
+
+//         console.log("Interval Stopped");
+
+//     }
+
+// }, 1000);
+
+// const clock = setInterval(function () {
+
+//     console.log(new Date().toLocaleTimeString());
+
+// }, 1000);
+
+// setTimeout(function () {
+
+//     clearInterval(clock);
+
+//     console.log("Clock Stopped");
+
+// }, 15000);
+
+
+// const promises = new Promise((res,rej)=>{
+//     res("Hii")
+// })
+// promises.then((data)=>{
+//     console.log(data);
+    
+    
+
+// })
+
+async function add(a,b) {
+    try {
+        const result = await a + b;
+        console.log(result);
+        
+    } catch (error) {
+        console.log(error);
+        
+    }
+    finally{
+        console.log("completed");
+        
+    }
+ }
+add('abcd ' );
